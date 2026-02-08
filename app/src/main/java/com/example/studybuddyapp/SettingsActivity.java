ce77f18be509bba509c7f2997c036154f9ac4354
Sybel Yadeen <aquillasybelyadeen123@gmail.com>
2026-02-08 14:05:44 +0800

    Commit 4: Added Buttons to Profile and Settings Page

--- app/src/main/java/com/example/studybuddyapp/SettingsActivity.java
+++ app/src/main/java/com/example/studybuddyapp/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.studybuddyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,18 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
+
+        // Logic for Dashboard Button
+        Button dashboardBtn = findViewById(R.id.btn_backto_dashboard);
+        dashboardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
